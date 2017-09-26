7f6c39673628de0ff78e69018147f36d4544396d
Cribbin <CribbinPJ@gmail.com>
2017-09-26 12:47:57 +0100

    Testing

--- src/main/java/Temp.java
+++ src/main/java/Temp.java
@@ -2,4 +2,8 @@ public class Temp {
     public String test(String name) {
         return "Hello" + name;
     }
+
+    pubic String goodBye(String name) {
+       return "Goodbye" + name; 
+    }
 }
