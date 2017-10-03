21f69552b0d501fc461938d9172e83f1e25b7c67
Tom-J-Barrett <motb141@gmail.com>
2017-10-03 23:19:21 +0100

    File implemented for adding hosts

--- src/main/java/fleetManager/hosts/Host.java
+++ src/main/java/fleetManager/hosts/Host.java
@@ -7,13 +7,50 @@ public class Host {
     private final String DNS;
     private String port;
     private boolean publicIP;
+    private String state;
 
     public Host(String state, String ipv4, String dns, String port, boolean publicIP){
         this.ipv4 = ipv4;
+        this.state=state;
         DNS = dns;
         this.port = port;
         this.publicIP=publicIP;
         hostStateFactory =new HostStateFactory();
         hostState= hostStateFactory.hostState(state);
+    }
+
+    public String getIpv4() {
+        return ipv4;
+    }
+
+    public String getDNS() {
+        return DNS;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public boolean isPublicIP() {
+        return publicIP;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getSubnet(){
+        if(publicIP)
+            return "public";
+        else
+            return "private";
     }
 }
--- src/main/java/fleetManager/storage/HostDAOFileSystem.java
+++ src/main/java/fleetManager/storage/HostDAOFileSystem.java
@@ -6,6 +6,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.List;
 
 public class HostDAOFileSystem implements HostDAO{
@@ -15,10 +16,20 @@ public class HostDAOFileSystem implements HostDAO{
     }
 
     public void addHost(Host host){
+        try
+        {
+            String filename= "Hosts.txt";
+            FileWriter fw = new FileWriter(filename,true); //the true will append the new data
+            fw.write("\n"+host.getIpv4()+","+host.getDNS()+","+host.getPort()+","+host.getState()+","+host.getSubnet());//appends the string to the file
+            fw.close();
+        }
+        catch(IOException ioe)
+        {
+            System.err.println("IOException: " + ioe.getMessage());
+        }
     }
 
     public void removeHost(Host host) {
-
     }
 
     public void enableHost(Host host) {
