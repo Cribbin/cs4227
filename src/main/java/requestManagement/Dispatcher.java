b83bbdf3d1d7c9c9b41df5df82c94345cf8f2609
Robbie Hoodless <Robbie Hoodless>
2017-10-05 21:23:33 +0100

    Set up Request Manager, Event Dispatcher and some associated tests.
    - Added RequestManager and its Builder.
    - Set up event dispatcher
    - Added some tests
    - Set up maven dependency file and added some needed dependencies.

--- src/main/java/requestManagement/Dispatcher.java
+++ src/main/java/requestManagement/Dispatcher.java
@@ -1,4 +1,38 @@
 package requestManagement;
 
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+
+import java.util.Collection;
+
+/**
+ * Dispatcher for interception services.
+ *
+ * This class maintains a list of all active services that have subscribed to listen for events subject to interception.
+ * It also handles dispatching the events to all of the subscribed services.
+ *
+ * Note: Ordered execution of third party defined services is supported by using any ordered collection to store
+ * the services (e.g. ArrayList).
+ */
 public class Dispatcher {
+    private Collection<Service> services;
+
+    public Dispatcher(Collection<Service> services) {
+        this.services = services;
+    }
+
+    /** Register for all dispatched events */
+    public void register(Service service) {
+        services.add(service);
+    }
+
+    /** This event is called as soon as the request is received by the framework. */
+    public void dispatchIncomingRequest(HttpRequest request, Context context) {
+        services.forEach( s -> s.processIncomingRequest(request, context));
+    }
+
+    /** This event is called prior to the HttpResponse being sent. */
+    public void dispatchOutgoingResponse(HttpResponse response, Context context) {
+        services.forEach(s -> s.processOutgoingResponse(response, context));
+    }
 }
--- src/main/java/requestManagement/RequestManagerImpl.java
+++ src/main/java/requestManagement/RequestManagerImpl.java
@@ -1,4 +1,44 @@
 package requestManagement;
 
-public class RequestManagerImpl {
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import requestManagement.fleet.FleetManager;
+import requestManagement.loadBalancing.LoadBalancer;
+
+public class RequestManagerImpl implements RequestManager {
+
+    private Context context = null;
+    private Dispatcher dispatcher = null;
+    private LoadBalancer loadBalancer = null;
+    private FleetManager fleetManager = null;
+
+    RequestManagerImpl(RequestManagerBuilder builder) {
+        context = builder.getContext();
+        dispatcher = builder.getDispatcher();
+        loadBalancer = builder.getLoadBalancer();
+        fleetManager = builder.getFleetManager();
+    }
+
+    @Override
+    public HttpResponse handleRequest(HttpRequest request) {
+
+        /* Dispatch incoming request event to all subscribed services. */
+        dispatcher.dispatchIncomingRequest(request, context);
+
+        /* Actually forward the request. This is where the Load Balancer would be called.*/
+
+        HttpResponse response = null;
+
+        /* response = loadBalancer.handleRequest(request); */
+
+        /* Dispatch outgoing response to all subscribed services */
+        dispatcher.dispatchOutgoingResponse(response, context);
+
+        return response;
+    }
+
+    @Override
+    public void initialiseFleet() {
+        /* Should initialise the fleet here eventually. */
+    }
 }
