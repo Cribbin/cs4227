0434a8ef9d34349bf50ced2c83ab692d63329e49
Cribbin <CribbinPJ@gmail.com>
2017-10-22 12:12:12 +0100

    Add tests for HealthCheckImplementation

---
+++ src/main/test/requestManagement/fleetManager/HealthCheckImplementationTest.java
@@ -0,0 +1,80 @@
+package requestManagement.fleetManager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import requestManagement.fleetManager.hosts.Host;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class HealthCheckImplementationTest {
+    private static final String LOOPBACK_IP = "127.0.0.1";
+    private static final String INVALID_IP = "999.999.999";
+    private static final String DNS = "testDns";
+    private static final int PORT_80 = 80;
+    private static final String ACTIVE_STATE = "active";
+    private static final String INACTIVE_STATE = "inactive";
+
+    private HealthCheckImplementation healthCheck;
+    private FleetManager fleetManager;
+
+    @BeforeEach
+    void setUp() {
+        fleetManager = mock(FleetManager.class);
+        healthCheck = new HealthCheckImplementation(fleetManager);
+    }
+
+    @Test
+    void shouldPingLocalHostAndUpdateState() {
+        Host localHost = getHost(LOOPBACK_IP, INACTIVE_STATE);
+        assertState(localHost, INACTIVE_STATE);
+
+        healthCheck.runHealthCheck();
+        assertState(localHost, ACTIVE_STATE);
+    }
+
+    @Test
+    void shouldPingLocalHostAndNotChangeActiveState() {
+        Host localHost = getHost(LOOPBACK_IP, ACTIVE_STATE);
+        assertState(localHost, ACTIVE_STATE);
+
+        healthCheck.runHealthCheck();
+        assertState(localHost, ACTIVE_STATE);
+    }
+
+    @Test
+    void shouldFailPingingNonExistingHostAndUpdateState() {
+        Host deadHost = getHost(INVALID_IP, ACTIVE_STATE);
+        assertState(deadHost, ACTIVE_STATE);
+
+        healthCheck.runHealthCheck();
+        assertState(deadHost, INACTIVE_STATE);
+    }
+
+    @Test
+    void shouldFailPingingNonExistingHostAndNotChangeInactiveState() {
+        Host deadHost = getHost(INVALID_IP, INACTIVE_STATE);
+        assertState(deadHost, INACTIVE_STATE);
+
+        healthCheck.runHealthCheck();
+        assertState(deadHost, INACTIVE_STATE);
+    }
+
+    private Host getHost(String ip, String state) {
+        Host h = new Host.HostBuilder(ip)
+                .withDns(DNS)
+                .withPort(PORT_80)
+                .withState(state)
+                .build();
+
+        when(fleetManager.getHosts()).thenReturn(Collections.singletonList(h));
+
+        return h;
+    }
+
+    private void assertState(Host host, String state) {
+        assert host.getState().toString().equals(state);
+    }
+}
