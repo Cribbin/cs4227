8c5d1e3c1f200eb5d171bc4e77262f5baad8208e
Cribbin <CribbinPJ@gmail.com>
2017-10-22 12:12:00 +0100

    Add implementation to HealthCheckImplementation

--- src/main/java/requestManagement/fleetManager/HealthCheckImplementation.java
+++ src/main/java/requestManagement/fleetManager/HealthCheckImplementation.java
@@ -1,18 +1,30 @@
 package requestManagement.fleetManager;
 
 import requestManagement.fleetManager.hosts.Host;
-
-import java.util.List;
+import java.net.InetAddress;
 
 public class HealthCheckImplementation implements HealthCheck {
-
     private FleetManager fleetManager;
-    private List<Host> allHosts;
 
-    public HealthCheckImplementation() {}
+    public HealthCheckImplementation(FleetManager fleetManager) {
+        this.fleetManager = fleetManager;
+    }
 
     @Override
     public void runHealthCheck() {
-        fleetManager = FleetManager.getInstance();
+        fleetManager.getHosts().forEach(this::individualHealthCheck);
+    }
+
+    private void individualHealthCheck(Host host) {
+        String ip = host.getIpv4();
+        try {
+            InetAddress inetAddress = InetAddress.getByName(ip);
+
+            host.setState(inetAddress.isReachable(5000) ? "active" : "inactive");
+
+        } catch (Exception ex) {
+            System.out.println("Exception:" + ex.getMessage());
+            host.setState("inactive");
+        }
     }
 }
